5391c8f765a35233a10893f9411921152c535750
ywon9909@naver.com <ywon9909@naver.com>
2020-12-01 18:18:06 +0900

    UTF-8

--- JavaGameClient/src/javaClientSetting.java
+++ JavaGameClient/src/javaClientSetting.java
@@ -15,72 +15,70 @@ import java.awt.Canvas;
 import java.awt.Color;
 
 public class javaClientSetting extends JFrame{
-	private JPanel contentPane;
-	private JColorChooser cc;
-	
-	public javaClientSetting() {
-		setTitle("¼³Á¤");
-		
-		setResizable(false);
-		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 254, 321);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(204, 255, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		
-		JButton btn = new JButton("´ëÈ­³»¿ë ³»º¸³»±â");
-		btn.setBackground(new Color(255, 255, 204));
-		btn.setBounds(59, 28, 146, 33);
-		contentPane.add(btn);
-		ChatTextAction chataction = new ChatTextAction();
-		btn.addActionListener(chataction);
-		
-		
-		JButton btn_1 = new JButton("Ã¤ÆÃ¹æ ¹è°æÈ­¸é");
-		btn_1.setBackground(new Color(255, 255, 204));
-		btn_1.setBounds(59, 94, 146, 33);
-		contentPane.add(btn_1);
-		BackgroundAction backaction = new BackgroundAction();
-		btn_1.addActionListener(backaction);
-	}
-	class BackgroundAction implements ActionListener{
+   private JPanel contentPane;
+   private JColorChooser cc;
+   
+   public javaClientSetting() {
+      setTitle("    ");
+      
+      setResizable(false);
+      //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(100, 100, 254, 321);
+      contentPane = new JPanel();
+      contentPane.setBackground(new Color(204, 255, 255));
+      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      setContentPane(contentPane);
+      contentPane.setLayout(null);
+      
+      
+      JButton btn = new JButton("ëŒ€í™”ë‚´ìš© ë‚´ë³´ë‚´ê¸°");
+      btn.setBackground(new Color(255, 255, 204));
+      btn.setBounds(59, 28, 146, 33);
+      contentPane.add(btn);
+      ChatTextAction chataction = new ChatTextAction();
+      btn.addActionListener(chataction);
+      
+      
+      JButton btn_1 = new JButton("ì±„íŒ…ë°© ë°°ê²½ìƒ‰ ë°”ê¾¸ê¸°");
+      btn_1.setBackground(new Color(255, 255, 204));
+      btn_1.setBounds(59, 94, 146, 33);
+      contentPane.add(btn_1);
+      BackgroundAction backaction = new BackgroundAction();
+      btn_1.addActionListener(backaction);
+   }
+   class BackgroundAction implements ActionListener{
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-
-			Color newColor = JColorChooser.showDialog(cc,"Color Chooser",javaClientSetting.this.getBackground());
-			JavaGameClientView.talkList.setBackground(newColor);
-			JavaGameClientView.talkList.repaint();
-		}
-		
-	}
-	class ChatTextAction implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			ListModel model = JavaGameClientView.talkList.getModel();
-			Object o = null;
-			for(int i=0; i<model.getSize(); i++) {
-				o = model.getElementAt(i);
-				//System.out.println(o + "\n");
-			}
-			File file = new File("text.txt");
-			try {
-				file.createNewFile();
-				FileWriter fw = new FileWriter(file, true);
-				BufferedWriter bw = new BufferedWriter(fw);
-					
-					bw.write(o.toString());
-					bw.close();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-					System.out.println("fail create file");
-				}
-			
-		}
-	}
-	
+      @Override
+      public void actionPerformed(ActionEvent e) {
+         Color newColor = JColorChooser.showDialog(cc,"Color Chooser",javaClientSetting.this.getBackground());
+         JavaGameClientView.talkList.setBackground(newColor);
+         JavaGameClientView.talkList.repaint();
+      }
+      
+   }
+   class ChatTextAction implements ActionListener {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+         ListModel model = JavaGameClientView.talkList.getModel();
+         Object o = null;
+         for(int i=0; i<model.getSize(); i++) {
+            o = model.getElementAt(i);
+            File file = new File("text.txt");
+            try {
+               file.createNewFile();
+               FileWriter fw = new FileWriter(file);
+               //FileWriter fw = new FileWriter(file, true);
+               BufferedWriter bw = new BufferedWriter(fw);
+               
+               bw.write(o.toString() + "\n\n");
+               bw.close();
+            } catch (IOException e1) {
+               // TODO Auto-generated catch block
+               e1.printStackTrace();
+               System.out.println("fail create file");
+            }
+         }
+      }
+   }
+   
 }
